ed84dd9a73f63ebe3317c47bfac0c762fd3379fc
Rahul <rahulpaditkar@gmail.com>
2023-12-28 11:49:22 +0530

    Hero_Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero.jsx'
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Hero />
     </>
   );
 }
